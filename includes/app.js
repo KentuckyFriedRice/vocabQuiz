b96c9d6c81265275aec604a1f891db05e11da81f
Matt <58800558+KentuckyFriedRice@users.noreply.github.com>
2024-10-07 14:29:39 +0900

    Update app.js
    restartQuiz reloads page

--- includes/app.js
+++ includes/app.js
@@ -202,21 +202,7 @@ function checkAnswer() {
 
 // Function to reset variables and show the start screen again.
 function restartQuiz() {
-    // Reset all variables to their initial states
-    flashcards = [];
-    currentCardIndex = 0;
-    score = 0;
-    hasTriedOnce = false;
-    missedQuestions = [];
-    selectedDecks = [];
-
-    // Clear any results content
-    document.getElementById('final-score').innerText = '';
-    document.getElementById('missed-questions').innerHTML = '';
-
-    // Hide the results container and show the start container
-    document.getElementById('results-container').style.display = 'none';
-    document.getElementById('start-container').style.display = 'block';
+    location.reload();
 }
 
 window.onload = function() {
