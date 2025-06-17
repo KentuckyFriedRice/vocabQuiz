3ff3dadb87fbcbcfdab50c89d59f69c039a34da6
Matt <ricecreamsandwich@gmail.com>
2025-06-17 15:17:32 +0900

    Update app.js
    Set Elements saved to localstorage

--- includes/app.js
+++ includes/app.js
@@ -279,6 +279,9 @@ window.onload = function() {
     //Setting options
     const savedOptions = JSON.parse(localStorage.getItem('selectedOptions'));
     console.log(savedOptions);
+    document.getElementById('shuffle').checked = savedOptions[0];
+    document.getElementById('punct').checked = savedOptions[1];
+    document.getElementById('caps').checked = savedOptions[2];
     
     if (savedDecks && savedDecks.length > 0) {
         selectedDecks = savedDecks;
